9ed8a84671663067839f9808a631a0a737360c7a
Steven Hicken <sahicken@gmail.com>
2024-03-04 18:25:19 -0800

    output array prep

--- assns/three/output_array.c
+++ assns/three/output_array.c
@@ -8,14 +8,3 @@ void output_my_array(double* my_array, int my_array_size) {
     }
     printf("\n");
 }
-
-/*int main() {
-    // Example array
-    int my_array[] = { 10, 20, 30, 40, 50 };
-    int array_size = sizeof(my_array) / sizeof(my_array[0]);
-
-    // Call the function
-    output_my_array(my_array, array_size);
-
-    return 0;
-}*/
