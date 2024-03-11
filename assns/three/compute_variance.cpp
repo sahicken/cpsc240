2371cb221d5326c4fa0438a4f56038a611fd2297
Steven Hicken <sahicken@gmail.com>
2024-03-11 16:22:58 -0700

    patch fxn name

--- assns/three/compute_variance.cpp
+++ assns/three/compute_variance.cpp
@@ -1,6 +1,6 @@
 #include <cmath>
 
-double calculateVariance(double arr[], int size, double mean) {
+double calculate_variance(double arr[], int size, double mean) {
     double variance = 0.0;
     for (int i = 0; i < size; i++) {
         variance += std::pow(arr[i] - mean, 2);
