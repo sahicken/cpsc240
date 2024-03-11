70b7348d1e4e8799a1eea827359c40307797be7f
Steven Hicken <sahicken@gmail.com>
2024-03-11 16:22:10 -0700

    compute mean boilerplate

---
+++ assns/three/compute_mean.asm
@@ -0,0 +1,124 @@
+;****************************************************************************************************************************
+; Program name: "Variance".  This program calculates the average driving time of a driver moving around NOC.     *
+;                                                                                                                            *
+; This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License  *
+; version 3 as published by the Free Software Foundation.  This program is distributed in the hope that it will be useful,   *
+; but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See   *
+; the GNU General Public License for more details A copy of the GNU General Public License v3 is available here:             *
+; <https://www.gnu.org/licenses/>.                                                                                           *
+;****************************************************************************************************************************
+
+
+
+;========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
+;Author information
+;  Author name: Steven Hicken
+;  Author email: sahicken@csu.fullerton.edu
+;
+;Program information
+;  Program name: Variance
+;  Programming languages: One module in C, one in X86, and one in bash.
+;  Date program began: 2024-Feb-19
+;  Date of last update: 2024-Feb-23
+;  Files in this program: driver.c, manager.asm, isfloat.asm, r.sh, input_array.asm, output_array.c
+;  Testing: working
+;  Status: alpha
+;
+;Purpose
+;  This program inputs arrays (double precision) and calculates variance
+;
+;This file:
+;  File name: manager.asm
+;  Language: X86-64
+;  Max page width: 124 columns
+;  Assemble (standard): nasm -l manager.lis -o manager.o manager.asm
+;  Assemble (debug): nasm -g dwarf -l manager.lis -o manager.o manager.asm
+;  Optimal print specification: Landscape, 7 points, monospace, 8½x11 paper
+;  Prototype of this function: double manager();
+; 
+;
+;
+;
+;========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
+
+;Declaration section, everything here does not have its own place of declaration
+
+global manager
+
+segment .data
+;This section (or segment) is for declaring initialized arrays
+
+segment .bss
+;This section (or segment) is for declaring empty arrays
+
+align 64
+; required for xstor and xrstor instructions
+backup_storage_area resb 832
+
+segment .text
+
+manager:
+
+;BEGIN .TEXT PREREQS
+; backup GPRs (General Purpose Registers)
+push rbp
+mov rbp, rsp
+push rbx
+push rcx
+push rdx
+push rdi
+push rsi
+push r8
+push r9
+push r10
+push r11
+push r12
+push r13
+push r14
+push r15
+pushf
+
+; backup all other registers (meaning not GPRs)
+mov rax,7
+mov rdx,0
+xsave [backup_storage_area]
+;END .TEXT PREREQS
+
+
+; use loop instruction and rcx
+
+
+
+;BEGIN .TEXT POSTREQS
+;Send back length of "third" side
+push qword 0
+push qword 0
+movsd [rsp], xmm15
+
+;Restore the values to non-GPRs
+mov rax,7
+mov rdx,0
+xrstor [backup_storage_area]
+
+movsd xmm0, [rsp]
+pop rax
+pop rax
+
+;Restore the GPRs
+popf
+pop r15
+pop r14
+pop r13
+pop r12
+pop r11
+pop r10
+pop r9
+pop r8
+pop rsi
+pop rdi
+pop rdx
+pop rcx
+pop rbx
+pop rbp   ;Restore rbp to the base of the activation record of the caller program
+ret
+;END .TEXT POSTREQS (BROKEN)
