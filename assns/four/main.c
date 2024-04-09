52eaebcfaac136a5b57badb6193d1d6b69ef8cc3
Steven Hicken <sahicken@gmail.com>
2024-04-08 22:43:08 -0700

    main impl

--- assns/four/main.c
+++ assns/four/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
-extern double manager();
+// should be const char*
+extern double executive();
 
 int main(void)
 {
-    printf("Welcome to Arrays of floating point numbers.\n");
-    printf("Brought to you by Steven Hicken.\n");
-    double var = manager();
-    printf("Main received this number %lf and will keep it for future use.\n", var);
-    printf("Main will return 0 to the operating system. Bye.\n");
+    printf("Welcome to Random Products, LLC.\n");
+    printf("This software is maintained by Steven Hicken.\n");
+    double name = executive();
+    printf("Oh, %lf. We hope you enjoyed your arrays. Do come again.\n", name);
+    printf("A zero will be returned to the operating system.\n");
     return 0;
 }
